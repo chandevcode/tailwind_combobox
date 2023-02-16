b403710e209ee51aef8689cdd354e1be4ebf4e2e
chan <baguschandradp@gmail.com>
2023-02-16 09:06:54 +0800

    starter

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './app/components/**/*.html.erb',
+    './app/components/**/*.erb',
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/assets/stylesheets/**/*.css',
+    './app/javascript/**/*.js'
+  ]
+}
